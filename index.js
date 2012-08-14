84e66c33def4c0ef1a413830a607e5e0bf06b5cd
Carlos Rodriguez <carlos@s8f.org>
2012-08-13 19:27:55 -0700

    experimental support for http-proxy

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ var amino = require('amino')
   , bouncy = require('bouncy')
   , cookie = require('cookie')
   , parseUrl = require('url').parse
+  , httpProxy = require('http-proxy')
 
-module.exports.createGateway = function(service, onError) {
+module.exports.createGateway = function (service, onError) {
   var stickyEnable = amino.get('stickyEnable')
     , stickyCookie = amino.get('stickyCookie')
     , stickyIP = amino.get('stickyIP')
     , stickyQuery = amino.get('stickyQuery')
+    , proxyModule = amino.get('proxyModule') || 'bouncy'
 
-  return bouncy(function(req, bounce) {
-    req.on('error', function(err) {
+  function setupRequest (req, cb) {
+    req.on('error', function (err) {
       console.error(err, '#error');
     });
-    var clientId
+    var clientId;
     if (stickyEnable) {
       if (stickyCookie && req.headers.cookie) {
         clientId = cookie.parse(req.headers.cookie)[stickyCookie];
@@ -33,22 +35,43 @@ module.exports.createGateway = function(service, onError) {
         }
       }
     }
-    var sReq = amino.requestService(service, req.headers['x-amino-version'], clientId);
+    req._sReq = amino.requestService(service, req.headers['x-amino-version'], clientId)
+    req._sReq.on('spec', cb);
+  }
 
-    sReq.on('spec', function(spec) {
-      bounce(spec.host, spec.port).on('error', function(err) {
-        var res = bounce.respond();
-        console.error(err, '#error on ' + spec);
-        sReq.emit('error', err);
-        if (onError) {
-          onError(err, req, res);
-        }
-        else {
-          res.writeHead(500, {'content-type': 'text/plain'});
-          res.write('Internal server error. Please try again later.');
-          res.end();
-        }
+  function onReqError (err, req, res, sReq, spec) {
+    console.error(err, '#error on ' + spec);
+    sReq.emit('error', err);
+    if (onError) {
+      onError(err, req, res);
+    }
+    else {
+      res.writeHead(500, {'content-type': 'text/plain'});
+      res.write('Internal server error. Please try again later.');
+      res.end();
+    }
+  }
+
+  if (proxyModule === 'http-proxy') {
+    var server = httpProxy.createServer(function (req, res, proxy) {
+      setupRequest(req, function (spec) {
+        req._spec = spec;
+        proxy.proxyRequest(req, res, {host: spec.host, port: spec.port});
       });
     });
-  });
+    server.proxy.on('proxyError', function (err, req, res) {
+      onReqError(err, req, res, req._sReq, req._spec);
+      return true;
+    });
+    return server;
+  }
+  else {
+    return bouncy(function (req, bounce) {
+      setupRequest(req, function (spec) {
+        bounce(spec.host, spec.port).on('error', function (err) {
+          onReqError(err, req, bounce.respond(), sReq, spec);
+        });
+      });
+    });
+  }
 };
